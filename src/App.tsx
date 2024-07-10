5a3e5cbc7baa25edd60ba55bbeed6698d647f270
valeryaosta <gerasimenko.valery@gmail.com>
2024-07-10 22:11:55 +0300

    feat: add custom hook for local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import { useState, useEffect } from 'react';
 import SearchBar from './components/searchbar/SearchBar';
 import CharacterList from './components/characterList/CharacterList';
 import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
 import ButtonWithError from './components/buttonWithBug/ButtonWithError';
 import FallbackContent from './components/fallbackContent/FallbackContent';
+import useLocalStorage from './hooks/useLocalStorage';
 import './App.css';
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState<string>(() => localStorage.getItem('searchTerm') || '');
+  const [searchTerm, setSearchTerm] = useLocalStorage('searchTerm', '');
 
-  useEffect(() => {
-    localStorage.setItem('searchTerm', searchTerm);
-  }, [searchTerm]);
-
-  const handleSearch = (searchTerm: string) => {
-    setSearchTerm(searchTerm);
-    if (searchTerm === '') {
-      localStorage.removeItem('searchTerm');
-    }
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
   };
 
   return (
