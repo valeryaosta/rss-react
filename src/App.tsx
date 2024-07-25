1c9b654f73373edbdf2efae9f842e04d25b46ee1
valeryaosta <gerasimenko.valery@gmail.com>
2024-07-25 14:53:10 +0300

    feat: add theme switcher with custom themes using context, update styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import FallbackContent from './components/fallbackContent/FallbackContent';
 import MainPage from './pages/mainPage/MainPage';
 import NotFoundPage from './pages/notFoundPage/NotFoundPage';
 import CharacterDetail from './components/characterDetail/CharacterDetail';
+import { useTheme } from './contexts/ThemeContext.tsx';
+import Light from '../src/assets/ligth-mode.png';
+import Dark from '../src/assets/dark-mode.png';
+import './App.css';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <ErrorBoundary fallback={<FallbackContent />}>
-      <Router>
-        <Routes>
-          <Route path='/' element={<MainPage />}>
-            <Route path='character/:characterId' element={<CharacterDetail />} />
-          </Route>
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </Router>
-    </ErrorBoundary>
+    <div className={`app ${theme}`}>
+      <ErrorBoundary fallback={<FallbackContent />}>
+        <Router>
+          <button onClick={toggleTheme} className='theme-toggle-btn'>
+            Theme {theme === 'light' ? <img src={Light} alt='Light mode' /> : <img src={Dark} alt='Dark mode' />}
+          </button>
+          <Routes>
+            <Route path='/' element={<MainPage />}>
+              <Route path='character/:characterId' element={<CharacterDetail />} />
+            </Route>
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </Router>
+      </ErrorBoundary>
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './store/store';
 import App from './App.tsx';
+import store from './store/store';
+import { ThemeProvider } from './contexts/ThemeContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
 );
